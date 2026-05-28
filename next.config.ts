c90813748810396b304904175726b62870cb8481
Khawatir <your_email@example.com>
2026-05-28 16:30:24 +0300

    update csp

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,3 @@
-// دي عشان نامن التشفير ابقي حتي في وضع لما نرفع الموقع فقط
-// upgrade-insecure-requests;
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 import withBundleAnalyzer from "@next/bundle-analyzer";
@@ -10,26 +8,19 @@ const withAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
 
-// 1. تعريف الـ Content Security Policy (الحصن الحصين)
-// ملاحظة: ضفنا كل الـ Hostnames بتاعة الصور اللي إنت مستخدمها عشان الـ CSP يسمح بيها
-
-//  لما تضيف بوابه دفع ابقي ارجع هنايا حمصه والا هتترفض
-//  ابقي استبدل دي بدومينات حقيقه يا حمصه
 const cspHeader = `
 default-src 'self';
 script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.paypal.com https://www.sandbox.paypal.com https://js.stripe.com https://www.gstatic.com;
 style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
 img-src 'self' blob: data: https://res.cloudinary.com avatars.githubusercontent.com lh3.googleusercontent.com images.unsplash.com via.placeholder.com placehold.co picsum.photos source.unsplash.com https://www.paypalobjects.com;
 font-src 'self' data: https://fonts.gstatic.com;
-connect-src 'self'  https://*.googleapis.com https://*.firebaseio.com  https://api.cloudinary.com https://www.paypal.com https://www.sandbox.paypal.com https://real-time-chat-for-zfashion-chat.up.railway.app wss://real-time-chat-for-zfashion-chat.up.railway.app;
+connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://api.cloudinary.com https://www.paypal.com https://www.sandbox.paypal.com https://real-time-chat-for-zfashion-chat.up.railway.app wss://real-time-chat-for-zfashion-chat.up.railway.app;
 frame-src 'self' https://www.paypal.com https://www.sandbox.paypal.com https://js.stripe.com;
 object-src 'none';
 base-uri 'self';
 form-action 'self' https://www.paypal.com https://www.sandbox.paypal.com;
-frame-ancestors 'none';
+frame-ancestors 'self' https://search.google.com;
 `;
-// دي عشان نامن التشفير ابقي حتي في وضع لما نرفع الموقع فقط
-// upgrade-insecure-requests;
 
 const nextConfig: NextConfig = {
   // إخفاء الـ Header اللي بيقول إننا شغالين Next.js (إخفاء الهوية)
@@ -48,7 +39,7 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  // 2. تطبيق كل الـ Security Headers (Helmet Style)
+  // 2. تطبيق كل الـ Security Headers
   async headers() {
     return [
       {
@@ -70,8 +61,9 @@ const nextConfig: NextConfig = {
             value: "max-age=63072000; includeSubDomains; preload",
           },
           {
+            // تم تحويلها لـ SAMEORIGIN لأن الـ CSP الحديث يعتمد على frame-ancestors وللسماح بالـ Framing الداخلي الآمن
             key: "X-Frame-Options",
-            value: "DENY",
+            value: "SAMEORIGIN",
           },
           {
             key: "X-Content-Type-Options",
